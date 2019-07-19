261d671fc25d3ff9f2b342d1cf0ff214308e651c
NavyaSurendran <navya.surendran9746@gmail.com>
2019-07-19 14:11:18 +1000

    Update UserNotFoundException.java

--- movie-cruiser/movie-cruiser-authentication/src/main/java/com/cognizant/moviecruiser/exception/UserNotFoundException.java
+++ movie-cruiser/movie-cruiser-authentication/src/main/java/com/cognizant/moviecruiser/exception/UserNotFoundException.java
@@ -1,5 +1,8 @@
 package com.cognizant.moviecruiser.exception;
-
+/**
+ * @author Navya Surendran
+ *
+ */
 public class UserNotFoundException extends Exception {
 	
 	public UserNotFoundException(String message) {
