8bc41fb526627f9bff390634781b0dcad4ed373b
NavyaSurendran <navya.surendran9746@gmail.com>
2019-07-19 14:08:06 +1000

    Update MovieRepository.java

--- movie-cruiser/movie-cruiser-backend/src/main/java/com/cognizant/moviecruiser/repo/MovieRepository.java
+++ movie-cruiser/movie-cruiser-backend/src/main/java/com/cognizant/moviecruiser/repo/MovieRepository.java
@@ -5,7 +5,10 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.cognizant.moviecruiser.domain.Movie;
-
+/**
+ * @author Navya Surendran
+ *
+ */
 public interface MovieRepository extends JpaRepository<Movie, Integer> {
 
 	List<Movie> findByUserId(String userId);
