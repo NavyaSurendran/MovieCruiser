59b2b27a4a946941abf05a96da1e1a60a5da333e
NavyaSurendran <navya.surendran9746@gmail.com>
2019-07-19 14:11:01 +1000

    Update UserAlreadyExsitsException.java

--- movie-cruiser/movie-cruiser-authentication/src/main/java/com/cognizant/moviecruiser/exception/UserAlreadyExsitsException.java
+++ movie-cruiser/movie-cruiser-authentication/src/main/java/com/cognizant/moviecruiser/exception/UserAlreadyExsitsException.java
@@ -1,5 +1,8 @@
 package com.cognizant.moviecruiser.exception;
-
+/**
+ * @author Navya Surendran
+ *
+ */
 public class UserAlreadyExsitsException extends Exception {
 	
 	public UserAlreadyExsitsException(String message) {
