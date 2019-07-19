1462559d5e19209decbf77c257e68b8ab785dbd8
NavyaSurendran <navya.surendran9746@gmail.com>
2019-07-19 14:11:44 +1000

    Update UserRepository.java

--- movie-cruiser/movie-cruiser-authentication/src/main/java/com/cognizant/moviecruiser/repo/UserRepository.java
+++ movie-cruiser/movie-cruiser-authentication/src/main/java/com/cognizant/moviecruiser/repo/UserRepository.java
@@ -3,7 +3,10 @@ package com.cognizant.moviecruiser.repo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.cognizant.moviecruiser.entity.User;
-
+/**
+ * @author Navya Surendran
+ *
+ */
 public interface UserRepository extends JpaRepository<User, String> {
 	
 	User findByUserIdAndPassword(String userId, String password);
