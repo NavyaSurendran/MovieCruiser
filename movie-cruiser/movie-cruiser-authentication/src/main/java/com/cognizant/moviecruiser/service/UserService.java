6dde4c39f7b89741a48322aea96d9b926e93f42b
NavyaSurendran <navya.surendran9746@gmail.com>
2019-07-19 14:12:57 +1000

    Update UserService.java

--- movie-cruiser/movie-cruiser-authentication/src/main/java/com/cognizant/moviecruiser/service/UserService.java
+++ movie-cruiser/movie-cruiser-authentication/src/main/java/com/cognizant/moviecruiser/service/UserService.java
@@ -3,7 +3,10 @@ package com.cognizant.moviecruiser.service;
 import com.cognizant.moviecruiser.entity.User;
 import com.cognizant.moviecruiser.exception.UserAlreadyExsitsException;
 import com.cognizant.moviecruiser.exception.UserNotFoundException;
-
+/**
+ * @author Navya Surendran
+ *
+ */
 public interface UserService {
 	
 	boolean saveUser (User user) throws UserAlreadyExsitsException, UserNotFoundException;
